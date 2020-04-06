605914096a06f18efbf59f672cf1967445b4f856
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2020-04-05 21:57:41 -0400

    deployment configs, continued

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useForm } from "react-hook-form";
 import Preview from "./components/Preview";
 import ConnectedTo from "./components/ConnectedTo";
-const socket = io("http://localhost:4321");
+const socket = io(`http://localhost:${process.env.PORT || 4321}`);
 
 const initialState = {
   stream: undefined,
